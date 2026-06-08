b86d1d1105e380b623d4bfa6b3bea6365ce5a1bb
fugitivemarginalia <audwayconrey@gmail.com>
2026-06-08 16:53:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 function App() {
   return (<>
   <section id="hero">
