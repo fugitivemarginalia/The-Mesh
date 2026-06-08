f092fa68bd0ed17dd57db24de695fa26da86c309
fugitivemarginalia <audwayconrey@gmail.com>
2026-06-08 13:25:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   <div className="map-placeholder">
     <MapContainer
   center={[52.703, -8.864]}
-  zoom={14}
+  zoom={18}
   scrollWheelZoom={false}
    doubleClickZoom={true}
   style={{ height: '100%', width: '100%' }}
