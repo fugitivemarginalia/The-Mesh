6c4316d6701b7098b27819890eb2113a4c84651b
fugitivemarginalia <audwayconrey@gmail.com>
2026-06-13 16:05:05 +0100

    buttons glow

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   <TileLayer
     attribution='&copy; OpenStreetMap contributors'
-    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
   />
 
 </MapContainer>
